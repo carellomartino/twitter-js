450c04b4a8ba04eea3adfa0e38ee708a65bfa06e
Nicolas <carellomartino@gmail.com>
2019-05-08 17:23:55 -0300

    actualizacion 17:23 hs

--- app.js
+++ app.js
@@ -1,37 +1,27 @@
 var express = require('express');
 var app = express();
-var nunjucks = require('nunjucks')
-
-app.get('/', function(req, res){
-    res.render( 'index', {title: 'Hall of Fame', people: locals.people});
-});
-
-app.listen(3000, function(){
-    console.log("Funciona en puerto 3000")
-});
-
-app.use(function (req, res, next){
-    console.log("Primer middleware")
-    next();
-})
-
-var locals = {
-    title: 'Ejemplo',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione' }
-    ]
-};
-
-app.set('view engine', 'html');
-app.engine('html', nunjucks.render);
-
+var nunjucks = require('nunjucks');
+var routes = require('./routes');
+var morgan = require('morgan')
+app.use('/', routes);
 
 nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, function (err, output) {
-    console.log(output);
+nunjucks.render('index.html', function (err, output){});
+
+app.use(morgan('tiny'))
+
+app.use(express.static('public'));
+
+app.use(function
+     (req, res, next){
+    console.log("Primer middleware")
+    next();
 });
 
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-//res
+app.set('view engine', 'html');
+
+app.engine('html', nunjucks.render);
+
+app.listen(4000, function(){
+    console.log("Funciona en puerto 4000")
+});
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var tweetBank = require('../tweetBank');
+router.get('/', function(req, res){
+    var tweets = tweetBank.list();
+    res.render('index', {tweets: tweets})
+});
+
+module.exports = router;
